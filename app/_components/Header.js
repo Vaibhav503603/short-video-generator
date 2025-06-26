a9bc91cb0de996edd6c1453cbab71a5c3ab1fb93
Vaibhav503603 <vaibhavsacheva2051@gmail.com>
2025-06-26 21:53:52 +0530

    Add initial project setup with Next.js, Tailwind CSS, and essential components

---
+++ app/_components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function header() {
+  return (
+    <div>
+
+        <image src={'/logo.svg'}
+        alt='logo'
+        width={30}
+        height={30}
+        />
+    </div>
+  )
+}
+
+export default header
